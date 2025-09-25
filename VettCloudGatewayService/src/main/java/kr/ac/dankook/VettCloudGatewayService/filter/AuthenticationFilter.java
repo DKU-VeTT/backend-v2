f8fa1d0c53d0e966a2a25e42ba18a6c4259a4992
ohchanKyu <okc0202@naver.com>
2025-09-26 00:33:32 +0900

    build: Modify gRPC build dependencies.

--- VettCloudGatewayService/src/main/java/kr/ac/dankook/VettCloudGatewayService/filter/AuthenticationFilter.java
+++ VettCloudGatewayService/src/main/java/kr/ac/dankook/VettCloudGatewayService/filter/AuthenticationFilter.java
@@ -33,7 +33,7 @@ public class AuthenticationFilter implements GlobalFilter {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 
         String path = exchange.getRequest().getURI().getPath();
-        if (path.startsWith("/api/v1/auth")){
+        if (path.startsWith("/api/v1/auth") || path.startsWith("/actuator")){
             return chain.filter(exchange);
         }
         String token = resolveToken(exchange);
